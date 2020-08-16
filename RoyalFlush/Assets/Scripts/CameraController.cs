0929e13be3a0d0b2b9b13edbcb74969a283bbe80
Colin Taylor-Adair <atniloc@gmail.com>
2020-08-16 16:12:41 -0500

    Simple Camera Controller
    Just follows the player for now, probably should give a small buffer and lerp to the position

---
+++ RoyalFlush/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 camPos = player.transform.position;
+        camPos.z = transform.position.z;
+        transform.position = camPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 camPos = player.transform.position;
+        camPos.z = transform.position.z;
+        transform.position = camPos;
+    }
+}
