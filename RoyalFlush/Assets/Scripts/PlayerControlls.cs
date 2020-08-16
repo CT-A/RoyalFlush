2c5e538cf9186c64f64d566ece6d578244610093
Redzango <cjohns10@trinity.edu>
2020-08-16 15:27:46 -0500

    player movement

---
+++ RoyalFlush/Assets/Scripts/PlayerControlls.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlls : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    public bool isMelee;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        Debug.Log(movement.x);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveSpeed * movement * Time.fixedDeltaTime);
+    }
+}
