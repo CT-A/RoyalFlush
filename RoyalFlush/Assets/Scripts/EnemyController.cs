bb5559b9568f791b76211e08c44d7f0278265e96
Colin Taylor-Adair <atniloc@gmail.com>
2020-08-16 20:49:46 -0500

    finished enemy pathfinding, kinda
    enemies will cause null reference exeptions instead of attacking, as they are still trying to pathfind when they hit the player.

--- RoyalFlush/Assets/Scripts/EnemyController.cs
+++ RoyalFlush/Assets/Scripts/EnemyController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    //remember to initialize grid in AStarPathfinding when spawning enemy
+    public GameObject player;
+    public AStarPathfinding pf;
+    public float speed;
+    public bool moving;
     // Start is called before the first frame update
     void Start()
     {
-        
+        moving = true;
+        pf = GetComponent<AStarPathfinding>();
+        player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (moving) move();
+    }
+
+    void move()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position,pf.WorldPointFromNode(pf.FindPath(transform.position,player.transform.position)[0]),step);
     }
 }
