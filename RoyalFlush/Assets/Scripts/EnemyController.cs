5016b126fadf207cb14968d655f5deb9c80e8660
Colin Taylor-Adair <atniloc@gmail.com>
2020-08-17 19:17:25 -0500

    Enemy Attacks if close enough to player
    Attack has a cooldown but doesnt do anything yet

--- RoyalFlush/Assets/Scripts/EnemyController.cs
+++ RoyalFlush/Assets/Scripts/EnemyController.cs
@@ -6,12 +6,16 @@ public class EnemyController : MonoBehaviour
 {
     //remember to initialize grid in AStarPathfinding when spawning enemy
     public GameObject player;
-    public AStarPathfinding pf;
-    public float speed;
-    public bool moving;
+    private AStarPathfinding pf;
+    private float speed;
+    private bool moving;
+    public float atkCD;
+    private float atkTime;
     // Start is called before the first frame update
     void Start()
     {
+        atkTime = 1;
+        speed = 2.5f;
         moving = true;
         pf = GetComponent<AStarPathfinding>();
         player = GameObject.FindWithTag("Player");
@@ -20,12 +24,41 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //try to attack
+        attack();
+
+        //if you're moving, move
         if (moving) move();
     }
 
     void move()
     {
         float step = speed * Time.deltaTime;
+        //new position is closer to the first step on the rout to the player
         transform.position = Vector3.MoveTowards(transform.position,pf.WorldPointFromNode(pf.FindPath(transform.position,player.transform.position)[0]),step);
+    }
+
+    void attack() {
+        //if not attacking and next to player, start
+        if ((pf.FindPath(transform.position, player.transform.position).Count == 0) && atkCD <= 0) {
+            atkCD = atkTime;
+            //stop moving
+            moving = false;
+            //start attack display
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
+
+        //decrement timer
+        atkCD -= Time.deltaTime;
+
+        //if done attacking, finish
+        if (atkCD <= 0)
+        {
+            //stop attack display
+            GetComponent<SpriteRenderer>().color = Color.white;
+            //start moving if far enough
+            if (pf.FindPath(transform.position, player.transform.position).Count > 0) moving = true;
+        }
+
     }
 }
